a303a014350583bd45640736d524fc705d5dac7e
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2022-12-05 22:07:14 -0300

    Update makelua.ps1

--- makelua.ps1
+++ makelua.ps1
@@ -4,6 +4,7 @@
 # makelua use tar
 # makelua use 7z
 
+$CURRENT_OS = 'Windows 11'
 $CURRENT_PATH = pwd
 $SCRIPT_PATH = $PSScriptRoot
 $MAKE_LUA_VERSION = '1.0.0'
@@ -11,7 +12,7 @@ $MAKE_LUA_VERSION = '1.0.0'
 cd $SCRIPT_PATH
 # makelua noone arg
 if($args.Count -eq 0){
-	Write-Host 'type: "makelua.ps1 help" for more information'
+	Write-Host 'type: "makelua help" for more information'
 	exit;
 }
 
@@ -20,6 +21,7 @@ if(($args.Count -ge 1) -and ($Args[0] -eq 'help')){
 	Write-Host "|MAKE_LUA HELP|
 	
 MakeLua info:
+ - os: $CURRENT_OS
  - path: `"$SCRIPT_PATH`"
  - version: $MAKE_LUA_VERSION
 
@@ -30,23 +32,23 @@ MakeLua uses:
  - tar
  - 7z
 
-MakeLua options: (link, compiler, otimization, lua_version, luarocks_version)
- - link:
- - compiler:
- - otimization:
+MakeLua options: (link, compiler, optimize, lua_version, luarocks_version)
+ - link: dynamic static
+ - compiler: msvc llvm gnu
+ - optimize: default size speed
  - lua_version:
  - luarocks_version:
 
 to install use `"makelua.ps1 dynamic msvc fast 5.4.4 3.9.1`"
-MakeLua is a installer"
+MakeLua is a installer";
 	exit;
 }
 
-# makelua options: (link, compiler, otimization, lua_version, luarocks_version)
+# makelua options: (link, compiler, optimize, lua_version, luarocks_version)
 if($args.Count -ge 1){
-	$IS_DYNAMIC_OR_STATIC = $Args[0] -as [string]; #msvc llvm gnu || compiler options	
+	$IS_DYNAMIC_OR_STATIC = $Args[0] -as [string]; #dynamic static || link options	
 } else {
-	$IS_DYNAMIC_OR_STATIC='dynamic'; # dynamic static || link options
+	$IS_DYNAMIC_OR_STATIC='dynamic';
 }
 if($args.Count -ge 2){
 	$COMPILER = $Args[1] -as [string]; #msvc llvm gnu || compiler options	
@@ -54,9 +56,9 @@ if($args.Count -ge 2){
 	$COMPILER = 'msvc';
 }
 if($args.Count -ge 3){
-	$OTIMIZATION = $Args[2] -as [string]; #default size speed || otimization options	
+	$OPTIMIZE = $Args[2] -as [string]; #default size speed || optimize options	
 } else {
-	$OTIMIZATION = 'default';
+	$OPTIMIZE = 'default';
 }
 if($args.Count -ge 4){
 	$LUA_VERSION = $Args[3] -as [string]; #lua version
@@ -145,6 +147,15 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 }';
 
 if ($COMPILER -eq 'msvc'){
+	if($OPTIMIZE -eq 'default'){
+		$O = 'Ot'
+	}
+	elseif($OPTIMIZE -eq 'speed'){
+		$O = 'O2'
+	}
+	elseif($OPTIMIZE -eq 'size'){
+		$O = 'O1'
+	}
 	$startEnv = $env:path;
 	function Invoke-VsScript {
 	  param(
@@ -170,62 +181,80 @@ if ($COMPILER -eq 'msvc'){
 	Invoke-VsScript vcvars64.bat;
 	echo 'using MSVC compiler';
 	echo "start build lua$LUA_VERSION_NAME.dll";
-	cl /O2 lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c /link /dll /out:lua$LUA_VERSION_NAME.dll | Out-Null;
+	cl /$O lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c /link /dll /out:lua$LUA_VERSION_NAME.dll | Out-Null;
 	echo "start build lua$LUA_VERSION_NAME.lib and liblua$LUA_VERSION_NAME.a";
-	cl /O2 /c lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c | Out-Null;
-	lib /O2 /out:lua$LUA_VERSION_NAME.lib lapi.obj lcode.obj lctype.obj ldebug.obj ldo.obj ldump.obj lfunc.obj lgc.obj llex.obj lmem.obj lobject.obj lopcodes.obj lparser.obj lstate.obj lstring.obj ltable.obj ltm.obj lundump.obj lvm.obj lzio.obj lauxlib.obj lbaselib.obj lcorolib.obj ldblib.obj liolib.obj lmathlib.obj loadlib.obj loslib.obj lstrlib.obj ltablib.obj lutf8lib.obj linit.obj | Out-Null;
+	cl /$O /c lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c | Out-Null;
+	lib /$O /out:lua$LUA_VERSION_NAME.lib lapi.obj lcode.obj lctype.obj ldebug.obj ldo.obj ldump.obj lfunc.obj lgc.obj llex.obj lmem.obj lobject.obj lopcodes.obj lparser.obj lstate.obj lstring.obj ltable.obj ltm.obj lundump.obj lvm.obj lzio.obj lauxlib.obj lbaselib.obj lcorolib.obj ldblib.obj liolib.obj lmathlib.obj loadlib.obj loslib.obj lstrlib.obj ltablib.obj lutf8lib.obj linit.obj | Out-Null;
 	cp lua$LUA_VERSION_NAME.lib liblua$LUA_VERSION_NAME.a;
 	echo "start build lua$LUA_VERSION_NAME.exe";
-	cl /O2 lua$LUA_VERSION_NAME.lib lua.c /link /subsystem:console /defaultlib:shell32.lib /defaultlib:user32.lib /defaultlib:kernel32.lib /out:lua$LUA_VERSION_NAME.exe | Out-Null;
+	cl /$O lua$LUA_VERSION_NAME.lib lua.c /link /subsystem:console /defaultlib:shell32.lib /defaultlib:user32.lib /defaultlib:kernel32.lib /out:lua$LUA_VERSION_NAME.exe | Out-Null;
 	echo "start build lua$LUA_VERSION_NAME.exe";
-	cl /O2 lua$LUA_VERSION_NAME.lib lua.c wmain.c /link /subsystem:windows /defaultlib:shell32.lib /defaultlib:user32.lib /defaultlib:kernel32.lib /out:wlua$LUA_VERSION_NAME.exe | Out-Null;
+	cl /$O lua$LUA_VERSION_NAME.lib lua.c wmain.c /link /subsystem:windows /defaultlib:shell32.lib /defaultlib:user32.lib /defaultlib:kernel32.lib /out:wlua$LUA_VERSION_NAME.exe | Out-Null;
 	echo "start build luac$LUA_VERSION_NAME.exe";
-	cl /O2 lua$LUA_VERSION_NAME.lib luac.c /link /subsystem:console /defaultlib:shell32.lib /defaultlib:user32.lib /defaultlib:kernel32.lib  /out:luac$LUA_VERSION_NAME.exe | Out-Null;
+	cl /$O lua$LUA_VERSION_NAME.lib luac.c /link /subsystem:console /defaultlib:shell32.lib /defaultlib:user32.lib /defaultlib:kernel32.lib  /out:luac$LUA_VERSION_NAME.exe | Out-Null;
 	echo 'finish build';
 	RestartEnv;
 } elseif ($COMPILER -eq 'llvm'){
+	if($OPTIMIZE -eq 'default'){
+		$O = 'O3'
+	}
+	elseif($OPTIMIZE -eq 'speed'){
+		$O = 'Ofast'
+	}
+	elseif($OPTIMIZE -eq 'size'){
+		$O = 'Oz'
+	}
 	echo 'using LLVM compiler';
 	echo "start build lua$LUA_VERSION_NAME.dll";
-	clang -O3 -DNDEBUG -static lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c -shared -o lua$LUA_VERSION_NAME.dll;
+	clang -$O -DNDEBUG -static lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c -shared -o lua$LUA_VERSION_NAME.dll;
 	echo "start build lua$LUA_VERSION_NAME.lib and liblua$LUA_VERSION_NAME.a";
-	clang -O3 -DNDEBUG -c lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c;
+	clang -$O -DNDEBUG -c lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c;
 	llvm-ar -rcs lua$LUA_VERSION_NAME.lib lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o ltm.o lundump.o lvm.o lzio.o lauxlib.o lbaselib.o lcorolib.o ldblib.o liolib.o lmathlib.o loadlib.o loslib.o lstrlib.o ltablib.o lutf8lib.o linit.o;
 	cp lua$LUA_VERSION_NAME.lib liblua$LUA_VERSION_NAME.a;
 	echo "start build lua$LUA_VERSION_NAME.exe";
-	clang -O3 -DNDEBUG -static lua$LUA_VERSION_NAME.lib lua.c -$('Wl,-subsystem:console') -$('Wl,-defaultlib:shell32.lib') -$('Wl,-defaultlib:user32.lib') -$('Wl,-defaultlib:kernel32.lib') -o lua$LUA_VERSION_NAME.exe;
+	clang -$O -DNDEBUG -static lua$LUA_VERSION_NAME.lib lua.c -$('Wl,-subsystem:console') -$('Wl,-defaultlib:shell32.lib') -$('Wl,-defaultlib:user32.lib') -$('Wl,-defaultlib:kernel32.lib') -o lua$LUA_VERSION_NAME.exe;
 	echo "start build wlua$LUA_VERSION_NAME.exe";
-	clang -O3 -DNDEBUG -static lua$LUA_VERSION_NAME.lib lua.c wmain.c -$('Wl,-subsystem:windows') -$('Wl,-defaultlib:shell32.lib') -$('Wl,-defaultlib:user32.lib') -$('Wl,-defaultlib:kernel32.lib') -o wlua$LUA_VERSION_NAME.exe;
+	clang -$O -DNDEBUG -static lua$LUA_VERSION_NAME.lib lua.c wmain.c -$('Wl,-subsystem:windows') -$('Wl,-defaultlib:shell32.lib') -$('Wl,-defaultlib:user32.lib') -$('Wl,-defaultlib:kernel32.lib') -o wlua$LUA_VERSION_NAME.exe;
 	echo "start build luac$LUA_VERSION_NAME.exe";
-	clang -O3 -DNDEBUG -static lua$LUA_VERSION_NAME.lib luac.c -$('Wl,-subsystem:console') -$('Wl,-defaultlib:shell32.lib') -$('Wl,-defaultlib:user32.lib') -$('Wl,-defaultlib:kernel32.lib') -o luac$LUA_VERSION_NAME.exe;
+	clang -$O -DNDEBUG -static lua$LUA_VERSION_NAME.lib luac.c -$('Wl,-subsystem:console') -$('Wl,-defaultlib:shell32.lib') -$('Wl,-defaultlib:user32.lib') -$('Wl,-defaultlib:kernel32.lib') -o luac$LUA_VERSION_NAME.exe;
 	echo 'finish build';
 } elseif ($COMPILER -eq 'gnu'){
+	if($OPTIMIZE -eq 'default'){
+		$O = 'O3'
+	}
+	elseif($OPTIMIZE -eq 'speed'){
+		$O = 'Ofast'
+	}
+	elseif($OPTIMIZE -eq 'size'){
+		$O = 'Oz'
+	}
 	echo 'using GNU compiler';
 	echo "start build lua$LUA_VERSION_NAME.dll";
-	gcc -O3 -DNDEBUG -static-libgcc -static lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c -shared -o lua$LUA_VERSION_NAME.dll;
+	gcc -$O -DNDEBUG -static-libgcc -static lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c -shared -o lua$LUA_VERSION_NAME.dll;
 	echo "start build lua$LUA_VERSION_NAME.lib and liblua$LUA_VERSION_NAME.a";
-	gcc -O3 -DNDEBUG -c lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c;
+	gcc -$O -DNDEBUG -c lapi.c lcode.c lctype.c ldebug.c ldo.c ldump.c lfunc.c lgc.c llex.c lmem.c lobject.c lopcodes.c lparser.c lstate.c lstring.c ltable.c ltm.c lundump.c lvm.c lzio.c lauxlib.c lbaselib.c lcorolib.c ldblib.c liolib.c lmathlib.c loadlib.c loslib.c lstrlib.c ltablib.c lutf8lib.c linit.c;
 	ar -rcs lua$LUA_VERSION_NAME.lib lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o ltm.o lundump.o lvm.o lzio.o lauxlib.o lbaselib.o lcorolib.o ldblib.o liolib.o lmathlib.o loadlib.o loslib.o lstrlib.o ltablib.o lutf8lib.o linit.o;
 	cp lua$LUA_VERSION_NAME.lib liblua$LUA_VERSION_NAME.a;
 	echo "start build lua$LUA_VERSION_NAME.exe";
 	if($IS_DYNAMIC_OR_STATIC -eq 'dynamic'){
-		gcc -O3 -DNDEBUG -static-libgcc -static lua.c lua$LUA_VERSION_NAME.dll -W -o lua$LUA_VERSION_NAME.exe;
+		gcc -$O -DNDEBUG -static-libgcc -static lua.c lua$LUA_VERSION_NAME.dll -W -o lua$LUA_VERSION_NAME.exe;
 	}
 	elseif($IS_DYNAMIC_OR_STATIC -eq 'static'){
-		gcc -O3 -DNDEBUG -static-libgcc -static lua.c -L. -Bstatic -$('llua' + $LUA_VERSION_NAME) -W -o lua$LUA_VERSION_NAME.exe;
+		gcc -$O -DNDEBUG -static-libgcc -static lua.c -L. -Bstatic -$('llua' + $LUA_VERSION_NAME) -W -o lua$LUA_VERSION_NAME.exe;
 	}
 	echo "start build wlua$LUA_VERSION_NAME.exe";
 	if($IS_DYNAMIC_OR_STATIC -eq 'dynamic'){
-		gcc -mwindows -O3 -DNDEBUG -static-libgcc -static lua.c lua$LUA_VERSION_NAME.dll -W -o wlua$LUA_VERSION_NAME.exe;
+		gcc -mwindows -$O -DNDEBUG -static-libgcc -static lua.c lua$LUA_VERSION_NAME.dll -W -o wlua$LUA_VERSION_NAME.exe;
 	}
 	elseif($IS_DYNAMIC_OR_STATIC -eq 'static'){
-		gcc -mwindows -O3 -DNDEBUG -static-libgcc -static lua.c -L. -Bstatic -$('llua' + $LUA_VERSION_NAME) -W -o wlua$LUA_VERSION_NAME.exe;
+		gcc -mwindows -$O -DNDEBUG -static-libgcc -static lua.c -L. -Bstatic -$('llua' + $LUA_VERSION_NAME) -W -o wlua$LUA_VERSION_NAME.exe;
 	}
 	echo "start build luac$LUA_VERSION_NAME.exe";
 	if($IS_DYNAMIC_OR_STATIC -eq 'dynamic'){
-		gcc -O3 -DNDEBUG -static-libgcc -static luac.c lua$LUA_VERSION_NAME.dll -W -o luac$LUA_VERSION_NAME.exe;
+		gcc -$O -DNDEBUG -static-libgcc -static luac.c lua$LUA_VERSION_NAME.dll -W -o luac$LUA_VERSION_NAME.exe;
 	}
 	elseif($IS_DYNAMIC_OR_STATIC -eq 'static'){
-		gcc -O3 -DNDEBUG -static-libgcc -static luac.c -L. -Bstatic -$('llua' + $LUA_VERSION_NAME) -W -o luac$LUA_VERSION_NAME.exe;
+		gcc -$O -DNDEBUG -static-libgcc -static luac.c -L. -Bstatic -$('llua' + $LUA_VERSION_NAME) -W -o luac$LUA_VERSION_NAME.exe;
 	}
 	echo 'finish build';
 } else {
